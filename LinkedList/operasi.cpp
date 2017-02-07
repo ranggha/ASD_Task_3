995c3595ea70f75996a2273173b174aac301f30f
adf-telkomuniv <adf.asd.telkomuniversity@gmail.com>
2017-02-07 10:20:42 +0700

    Update operasi.cpp

--- LinkedList/operasi.cpp
+++ LinkedList/operasi.cpp
@@ -2,21 +2,6 @@
 #include "operasi.h"
 
 
-address sentinelSearch(List L, infotype x){
-    /**
-    * IS : List mungkin kosong
-    * PR : mencari elemen dengan ID infotype = infotype x menggunakan bantuan sentinel
-    * FS : mengembalikan address elemen dengan ID infotype x jika ditemukan,
-    *      mengembalikan NULL jika tidak ditemukan
-    */
-    //-------------your code here-------------
-    // NIM : 
-
-
-    //----------------------------------------
-
-}
-
 void insertionSort(List &L){
     /**
     * IS : List mungkin kosong
